9dab88f7b30495c3e4b0a7f292a9321c971151bf
Roman Petlitksiy <jeremy_wade@mail.ru>
2024-12-28 23:16:55 +0100

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 
 
 createRoot(document.getElementById('root')).render(
-    <HashRouter basename="/todo-app-redev">
+    <HashRouter>
         <App />
     </HashRouter>
 )
